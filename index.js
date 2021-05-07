1aab0ddbef84953ae7d78548861adbc699b1061a
hamzashamoun96 <hamza.shamoun96@gmail.com>
2021-05-07 17:17:12 +0300

    rockk

---
+++ index.js
@@ -0,0 +1,149 @@
+"use strict";
+
+require("dotenv").config();
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const io = require("socket.io")(server);
+const port = process.env.PORT || 4000;
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+let usersArr = [];
+for (let i = 0; i < 10; i++) {
+  usersArr.push([]);
+}
+
+let userRoom;
+let resRoom;
+let valid = true;
+let AvailableRooms = " ";
+let gameArr = [];
+let idxP1;
+let idxP2;
+
+io.on("connection", (socket) => {
+  socket.on("join", (username, room, callback) => {
+    addUser(username, socket.id, room);
+    if (valid) {
+      socket.join(room);
+      resRoom = room;
+      io.to(room).emit("onlineusers", usersArr[room - 1]);
+    } else {
+      getRandomRoom(usersArr);
+      return callback("This Room Is Full", AvailableRooms);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    usersAfterDisconnet(socket.id);
+    io.to(userRoom).emit("onlineusers", usersArr[userRoom - 1]);
+  });
+
+  socket.on("playeroneresult", (playerResult, player) => {
+    gameArr.push({
+      hand: playerResult.split(".")[0],
+      player,
+    });
+    if (gameArr.length === 2) {
+      idxP1 = gameArr.findIndex((e) => (e.player === "player 1" ? 1 : 0));
+      idxP2 = gameArr.findIndex((e) => (e.player === "player 2" ? 1 : 0));
+
+      let finalResult = calcResult(gameArr[idxP1].hand, gameArr[idxP2].hand);
+      io.to(resRoom).emit("gameresults", finalResult, gameArr);
+      gameArr = [];
+    }
+  });
+
+  socket.on("playertworesult", (playerResult, player) => {
+    gameArr.push({
+      hand: playerResult.split(".")[0],
+      player,
+    });
+
+    if (gameArr.length === 2) {
+      idxP1 = gameArr.findIndex((e) => (e.player === "player 1" ? 1 : 0));
+      idxP2 = gameArr.findIndex((e) => (e.player === "player 2" ? 1 : 0));
+
+      let finalResult = calcResult(gameArr[idxP1].hand, gameArr[idxP2].hand);
+      io.to(resRoom).emit("gameresults", finalResult, gameArr);
+      gameArr = [];
+    }
+  });
+});
+
+/*===================SOCKET_FUNCTIONS========================*/
+
+function addUser(username, socketId, room) {
+  if (usersArr[room - 1].length < 2) {
+    usersArr[room - 1].push({
+      username,
+      id: socketId,
+      room,
+    });
+    return (valid = true);
+  } else {
+    return (valid = false);
+  }
+}
+function usersAfterDisconnet(socketId) {
+  let idx;
+  usersArr.map((e) => {
+    idx = e.findIndex((i) => {
+      return i.id === socketId;
+    });
+    if (!(idx === -1)) {
+      userRoom = e[idx].room;
+      e.splice(idx, 1);
+    }
+  });
+  return userRoom;
+}
+function getRandomRoom(usersArr) {
+  usersArr.map((arr, idx) => {
+    if (arr.length < 2) {
+      AvailableRooms += (idx + 1).toString() + " ";
+    }
+  });
+  return AvailableRooms;
+}
+/*===================CALCULATE_RESULT========================*/
+function calcResult(hand1, hand2) {
+  if (hand1 === "none" && hand2 !== "none") return "Player 2 Win";
+  if (hand2 === "none" && hand1 !== "none") return "Player 1 Win";
+  if (hand1 === "none" && hand2 === "none") return "Draw";
+  if (hand1 === "rock" && hand2 === "rock") return "Draw";
+  if (hand1 === "rock" && hand2 === "paper") return "Player 2 Win";
+  if (hand1 === "rock" && hand2 === "scissors") return "Player 1 Win";
+  if (hand1 === "paper" && hand2 === "rock") return "Player 1 Win";
+  if (hand1 === "paper" && hand2 === "paper") return "Draw";
+  if (hand1 === "paper" && hand2 === "scissors") return "Player 2 Win";
+  if (hand1 === "scissors" && hand2 === "rock") return "Player 2 Win";
+  if (hand1 === "scissors" && hand2 === "paper") return "Player 1 Win";
+  if (hand1 === "scissors" && hand2 === "scissors") return "Draw";
+}
+/*===================RUN_THE_SERVER==========================*/
+const options = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+};
+
+mongoose
+  .connect(process.env.MONGODB_URI, options)
+  .then(() => {
+    // Start the web server
+    console.log("connected to MongoDB....!!");
+    server.listen(port, () => {
+      console.log(`working on ${port}`);
+    });
+  })
+  .catch((e) => {
+    throw new Error(e.message);
+  });
---
+++ public/app.js
@@ -0,0 +1,129 @@
+"use strict";
+
+let counter = 10;
+let countInterval;
+let buttons = ["rock", "paper", "scissors"];
+let playerOneResult;
+let playerTwoResult;
+
+const socket = io();
+
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
+});
+
+if (username && room) {
+  socket.emit("join", username, room, (error, AvailableRooms) => {
+    alert(error);
+    alert(`The Available Rooms Are ${AvailableRooms}`);
+    location.href = "/home.html";
+  });
+}
+let player1;
+let player2;
+socket.on("onlineusers", (usersArr) => {
+  $(".usersOnline").empty();
+  $(".usersOnline").append("<h2>Online Users :</h2>");
+  usersArr.forEach((e) => {
+    $(".usersOnline").append(`<p>${e.username}</p>`);
+  });
+
+  if (usersArr.length === 2) {
+    player1 = usersArr[0].username;
+    player2 = usersArr[1].username;
+
+    countInterval = setInterval(timer, 1000);
+    if (username === player1) {
+      // console.log('MUST_BE_PLAYER_1',player1,username)
+
+      for (let i = 0; i < buttons.length; i++) {
+        $(`#${buttons[i]}Btn`).on("click", () => {
+          $("#player1").css("background-image", `url(./img/${buttons[i]}.png)`);
+        });
+      }
+    } else if (username !== player1) {
+      for (let i = 0; i < buttons.length; i++) {
+        $(`#${buttons[i]}Btn`).on("click", () => {
+          $("#player2").css("background-image", `url(./img/${buttons[i]}.png)`);
+        });
+      }
+    }
+  }
+});
+
+socket.on("gameresults", (result, gameArr) => {
+  let idxP1 = gameArr.findIndex((e) => (e.player === "player 1" ? 1 : 0));
+  let idxP2 = gameArr.findIndex((e) => (e.player === "player 2" ? 1 : 0));
+  
+  if(gameArr[idxP1].hand === "none" && gameArr[idxP2].hand !== "none"){
+    $("#player1").css(
+      "background-image",
+      `url(./img/noselect.png)`
+    );
+    $("#player2").css(
+      "background-image",
+      `url(./img/${gameArr[idxP2].hand}.png)`
+    );
+  }else if(gameArr[idxP2].hand === "none" && gameArr[idxP1].hand !== "none"){
+    $("#player1").css(
+      "background-image",
+      `url(./img/${gameArr[idxP1].hand}.png)`
+    );
+    $("#player2").css(
+      "background-image",
+      `url(./img/noselect.png)`
+    );
+  }else if (gameArr[idxP1].hand === "none" && gameArr[idxP2].hand === "none"){
+    $("#player1").css(
+      "background-image",
+      `url(./img/noselect.png)`
+    );
+    $("#player2").css(
+      "background-image",
+      `url(./img/noselect.png)`
+    );
+  }else{
+    $("#player1").css(
+      "background-image",
+      `url(./img/${gameArr[idxP1].hand}.png)`
+    );
+    $("#player2").css(
+      "background-image",
+      `url(./img/${gameArr[idxP2].hand}.png)`
+    );
+  }
+  
+ 
+
+  $(".result").text(result);
+  $(".result").show();
+  setTimeout(() => {
+    // location.href = "/home.html";
+  }, 5000);
+});
+
+function timer() {
+  $(".timer").empty();
+  $(".timer").append(`<h3>${counter} s</h3>`);
+  if (counter === 0) {
+    clearInterval(countInterval);
+
+    playerOneResult = $("#player1").css("background-image").slice(31, -1);
+    playerTwoResult = $("#player2").css("background-image").slice(31, -1);
+
+    if (username === player1) {
+      if (playerOneResult !== "") {
+        socket.emit("playeroneresult", playerOneResult, "player 1");
+      } else {
+        socket.emit("playeroneresult", "none", "player 1");
+      }
+    } else if (username === player2) {
+      if (playerTwoResult !== "") {
+        socket.emit("playertworesult", playerTwoResult, "player 2");
+      } else {
+        socket.emit("playertworesult", "none", "player 2");
+      }
+    }
+  }
+  counter--;
+}
